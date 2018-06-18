fb9e97441bd090b98176f862382d810edfeae4e6
Mason <mguy2390@floridapoly.edu>
2018-06-18 01:43:58 -0400

    AForge References, Added Start and Stop Button, Added CaptureDevice Combo Box.
    Added functionality to Start and Stop Buttons.
    
    Fixed Overlapping Video bug caused by Starting both videos.

---
+++ Motion_Detection/Motion_Detection/MotionDetection.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Motion_Detection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.comboBoxSourceSelector = new System.Windows.Forms.ComboBox();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(28, 45);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(996, 480);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(492, 474);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(31, 558);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(173, 52);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // comboBoxSourceSelector
+            // 
+            this.comboBoxSourceSelector.FormattingEnabled = true;
+            this.comboBoxSourceSelector.Location = new System.Drawing.Point(31, 642);
+            this.comboBoxSourceSelector.Name = "comboBoxSourceSelector";
+            this.comboBoxSourceSelector.Size = new System.Drawing.Size(292, 28);
+            this.comboBoxSourceSelector.TabIndex = 2;
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(230, 558);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(173, 52);
+            this.stopButton.TabIndex = 3;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(1058, 757);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.comboBoxSourceSelector);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Motion Detection";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.ComboBox comboBoxSourceSelector;
+        private System.Windows.Forms.Button stopButton;
+    }
+}
+
---
+++ Motion_Detection/Motion_Detection/MotionDetection.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge.Imaging.Filters;
+using AForge.Imaging;
+using AForge.Vision;
+
+
+
+namespace Motion_Detection
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private VideoCaptureDevice videoSource;
+        private FilterInfoCollection captureDevice;
+        Boolean video = false;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            captureDevice = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo Device in captureDevice)
+            {
+                comboBoxSourceSelector.Items.Add(Device.Name);
+            }
+            comboBoxSourceSelector.SelectedItem = 0;
+            videoSource = new VideoCaptureDevice();
+            
+            
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if (video == true)
+            {
+
+                videoSource.NewFrame += new NewFrameEventHandler(videoSource_StopFrame);
+                videoSource.Stop();
+                videoSource = new VideoCaptureDevice(captureDevice[comboBoxSourceSelector.SelectedIndex].MonikerString);
+                videoSource.NewFrame += new NewFrameEventHandler(videoSource_NewFrame);
+                videoSource.Start();                
+            }
+            else
+            {
+
+
+                videoSource = new VideoCaptureDevice(captureDevice[comboBoxSourceSelector.SelectedIndex].MonikerString);
+                videoSource.NewFrame += new NewFrameEventHandler(videoSource_NewFrame);
+                videoSource.Start();
+                video = true;
+            }
+
+
+
+        }
+
+        private void videoSource_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+
+            pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone();
+
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            video = false;
+            videoSource.NewFrame += new NewFrameEventHandler(videoSource_StopFrame);
+            videoSource.Stop();
+        }
+
+        private void videoSource_StopFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+
+         pictureBox1.Image = null;
+         
+        }
+
+
+    }
+}
